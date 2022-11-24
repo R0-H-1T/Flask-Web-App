e232ab87f19c3ef119738d1dd062ddd13f3a4d6a
Rohit Tank <rowhittank@gmail.com>
2022-11-24 18:23:38 +0530

    Loading database. Create db if not already exists

--- website/__init__.py
+++ website/__init__.py
@@ -1,12 +1,36 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = 'fhuewf frbeuwobfr'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    db.init_app(app)
+
 
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+
+    # from . import models
+    from .models import User, Note  # The reason y we are importing is to load the db schema
+    
+    with app.app_context():
+        db.create_all()
+
     return app
+
+
+''' Flask-SQLAlchemy 3 no longer accepts an app argument to methods like create_all '''
+# def create_database(app):
+#     if not path.exists('website/' + DB_NAME):
+#         db.create_all(app=app)
+#         print('Created Database!')
