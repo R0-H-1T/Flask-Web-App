f4804538ef2db26a1b2962aa40ffd4dac5ad5f69
Rohit Tank <rowhittank@gmail.com>
2022-11-24 18:58:53 +0530

    vrify if user already exists(sign up), insert new user in db(sign up), fetch user record from db(login)

--- website/auth.py
+++ website/auth.py
@@ -1,13 +1,27 @@
 # this file will have bunch of routes inside 
 # all route urls
-from flask import Blueprint, render_template, request, flash
-
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        # user validation. Querying [look by specific entry in the db]
+        user = User.query.filter_by(email = email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again !', category='error')
+        else:
+            flash('Email does not exist.', category='error')
     return render_template('login.html', boolean=True)
 
 @auth.route('/logout')
@@ -18,20 +32,27 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4:
+        user = User.query.filter_by(email = email).first() # check is the user already exits. by email
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
             flash('Email must be greater than 4 characters.', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First Name must be greater than 1 character.', category='error')
         elif len(password1) < 7:
             flash('Password must be atlest 6 characters.', category='error')
         elif password1 != password2:
             flash('Password don\'t match', category='error')
         else:
+            new_user = User(email = email, first_name = first_name, password = generate_password_hash(password1, method='sha256')) # sha256 =>hashing algo
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created', category='success')
+            return redirect(url_for('views.home'))
 
 
     return render_template('sign_up.html')
